4b8731ef1ba4067601771473b06cf9fa0bc49ab3
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 08:52:07 +0200

    Update Trainer.java

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Trainer {
+public class Trainer {
+    private String name;
+    private String lizenzLevel;
+    private int erfahrung;
+
+    // Konstruktor
+    public Trainer(String name, String lizenzLevel, int erfahrung) {
+        this.name = name;
+        this.lizenzLevel = lizenzLevel;
+        this.erfahrung = erfahrung;
+    }
+
+    // Getter und Setter für die Attribute des Trainers
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLizenzLevel() {
+        return lizenzLevel;
+    }
+
+    public void setLizenzLevel(String lizenzLevel) {
+        this.lizenzLevel = lizenzLevel;
+    }
+
+    public int getErfahrung() {
+        return erfahrung;
+    }
+
+    public void setErfahrung(int erfahrung) {
+        this.erfahrung = erfahrung;
+    }
 }
