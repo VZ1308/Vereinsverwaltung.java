b2739f3c65ad6cad5bc033170dc3de518def68fb
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 08:51:48 +0200

    Update Spieler.java

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Spieler {
+public class Spieler {
+    private String name;
+    private String geburtsdatum;
+    private String position;
+    private int rueckennummer;
+
+    // Konstruktor
+    public Spieler(String name, String geburtsdatum, String position, int rueckennummer) {
+        this.name = name;
+        this.geburtsdatum = geburtsdatum;
+        this.position = position;
+        this.rueckennummer = rueckennummer;
+    }
+
+    // Getter und Setter für die Attribute des Spielers
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeburtsdatum() {
+        return geburtsdatum;
+    }
+
+    public void setGeburtsdatum(String geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getRueckennummer() {
+        return rueckennummer;
+    }
+
+    public void setRueckennummer(int rueckennummer) {
+        this.rueckennummer = rueckennummer;
+    }
 }
